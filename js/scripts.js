c81e773683a379654927fbe3fe3cc27621ad1d20
MaGloCon <mgcontrada@gmail.com>
2024-02-13 16:24:30 +0100

    Updated script.js with console.log for objects

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,19 @@ let bulbasaur = {
     height: 7,
     type: ['grass', 'poison']
 };
+console.log(bulbasaur);
+
 let charmander= {
     name: 'Charmander',
     height: 6,
     type:['fire']
 };
+console.log(charmander);
+
 let pikachu= {
     name: 'Pikachu',
     height: 0.4,
     type: ['electric']
 };
+
+console.log(pikachu);
