969199f5bc37523ecc26fdfb031c74dc9dfd78ce
MaGloCon <mgcontrada@gmail.com>
2024-02-13 16:15:18 +0100

    Updated script.js and created pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-alert ('Hello world');
-
-let favoriteFood= 'sushi';
-document.write(favoriteFood);
+let pokemonList = [];
+let bulbasaur = {
+    name: 'Bulbasaur',
+    height: 7,
+    type: ['grass', 'poison']
+};
+let charmander= {
+    name: 'Charmander',
+    height: 6,
+    type:['fire']
+};
+let pikachu= {
+    name: 'Pikachu',
+    height: 0.4,
+    type: ['electric']
+};
