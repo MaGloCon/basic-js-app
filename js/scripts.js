79c07089b744cfb3027f441fc27da93ba6b4e5ff
MaGloCon <mgcontrada@gmail.com>
2024-02-21 12:31:35 +0100

    Updated pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,16 @@
-let pokemonList = [];
-let bulbasaur = {
+let pokemonList = [{
     name: 'Bulbasaur',
     height: 7,
     type: ['grass', 'poison']
-};
-console.log(bulbasaur);
-
-let charmander= {
+},
+{
     name: 'Charmander',
     height: 6,
     type:['fire']
-};
-console.log(charmander);
-
-let pikachu= {
+},
+{
     name: 'Pikachu',
     height: 0.4,
     type: ['electric']
-};
-
-console.log(pikachu);
+},
+];
