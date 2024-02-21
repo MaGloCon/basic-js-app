632972178b1bfbaa5e057d584a678dae29bce067
MaGloCon <mgcontrada@gmail.com>
2024-02-21 12:40:45 +0100

    Revert updated pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,13 @@ let pokemonList = [{
     name: 'Bulbasaur',
     height: 7,
     type: ['grass', 'poison']
-};
-console.log(bulbasaur);
-
-let charmander= {
+},
+{
     name: 'Charmander',
     height: 6,
     type:['fire']
-};
-console.log(charmander);
-
-let pikachu= {
+},
+{
     name: 'Pikachu',
     height: 0.4,
     type: ['electric']
